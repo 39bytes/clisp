5fb4dc73126f6bff3ebb184d4cc3f113c26e6da4
39bytes <zhangjeff2014@gmail.com>
2024-06-11 18:18:02 -0400

    setup parsing with mpc

--- justfile
+++ justfile
@@ -1,2 +1,6 @@
 run:
-    cc -std=c99 -Wall -Wpedantic -Werror main.c -ledit -o lispy && ./lispy
+    cc -std=c99 -Wall -Wpedantic -Werror \
+        vendor/mpc.c main.c \
+        -ledit -lm \
+        -o lispy \
+    && ./lispy
--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <editline/readline.h>
 #include <histedit.h>
+#include "vendor/mpc.h"
 
 #ifdef _WIN32
 #include <string.h>
@@ -19,15 +20,41 @@ char *readline(char* prompt) {
 void add_history(char* unused) {}
 #endif
 
+
 int main(int argc, char** argv) {
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+
+    
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                                      \
+            number   : /-?[0-9]+/ '.' /[0-9]+/ | /-?[0-9]+/ ;  \
+            operator : '+' | '-' | '*' | '/' | '%' ;           \
+            expr     : <number> | '(' <operator> <expr>+ ')' ; \
+            lispy    : /^/ <operator> <expr>+ /$/ ;            \
+        ",
+        Number, Operator, Expr, Lispy);
+
     puts("Lispy version 0.1.0");
 
     while (true) {
         char *input = readline("~> ");
+        mpc_result_t r;
+
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
         add_history(input);
-        printf("Echo: %s\n", input);
         free(input);
     }
-
+    
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
     return 0;
 }
